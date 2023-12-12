c7b6f30ec175a56b2506ab2ca212d3a06ed1529b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-12 12:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const makeChange(c){
+const makeChange = (c) => {
   // Define the values of each coin
   const quarterValue = 0.25;
   const dimeValue = 0.10;
@@ -8,25 +8,36 @@ const makeChange(c){
   // Initialize variables to store the number of each coin
   let q = 0, d = 0, n = 0, p = 0;
 
+  // Convert the input string to a number
+  const amount = parseFloat(c);
+
+  // Check if the input is a valid number
+  if (isNaN(amount) || amount <= 0 || amount >= 100) {
+    return { error: "Invalid input. Please enter a positive number less than 100." };
+  }
+
   // Calculate the number of quarters
-  q = Math.floor(c / quarterValue);
-  c %= quarterValue;
+  q = Math.floor(amount / quarterValue);
+  amount %= quarterValue;
 
   // Calculate the number of dimes
-  d = Math.floor(c / dimeValue); 
-  c %= dimeValue;
+  d = Math.floor(amount / dimeValue);
+  amount %= dimeValue;
 
   // Calculate the number of nickels
-  n = Math.floor(c / nickelValue);
-  c %= nickelValue;
+  n = Math.floor(amount / nickelValue);
+  amount %= nickelValue;
 
   // The remaining amount is the number of pennies
-  p = Math.round(c / pennyValue);
+  p = Math.round(amount / pennyValue);
 
   // Create and return the result object
   const result = { q, d, n, p };
   return result;
-}
-// Do not the change the code below
+};
+
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
+
